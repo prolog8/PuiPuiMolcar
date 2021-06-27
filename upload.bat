ed080f20eb098263ddec89d16c74ed087f47b7e1
prolog8 <prolog8@gmail.com>
2021-06-27 17:59:06 +0800

    Add files via upload

---
+++ upload.bat
@@ -0,0 +1,1 @@
+C:\esptool_py\2.6.1\esptool.exe --chip esp32 --port COM3 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_dio_80m.bin 0x10000 m5_pui_tama.ino.bin 0x8000 m5_pui_tama.ino.partitions.bin 
